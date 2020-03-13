5fa9818c55f2805fda4f0c580c3b134e048d4b8f
Damaso Salgado Cassiani <dasalgado@PSLT5396.local>
2020-03-12 22:26:11 -0500

    modificacion CommandLineRunner

--- src/main/java/co/com/gestorsalas/GestorSalasApplication.java
+++ src/main/java/co/com/gestorsalas/GestorSalasApplication.java
@@ -16,20 +16,20 @@ public class GestorSalasApplication {
 		SpringApplication.run(GestorSalasApplication.class, args);
 	}
 	
-	 @Bean
-	    CommandLineRunner init(SalasRepository repository){
-	        return args -> {
+	@Bean
+    CommandLineRunner init(SalasRepository repository){
+        return args -> {
 
-	            Flux<Sala> books = Flux.just(
-	                    Sala.builder().capacidad("200").horaDisponible("08:00:00")
-	                    .nombre("Sala1").piso("2").build(),
-	                    Sala.builder().capacidad("200").horaDisponible("10:00:00")
-	                    .nombre("Sala2").piso("1").build()
-	            ).flatMap(p-> repository.save(p));
+            Flux<Sala> salas = Flux.just(
+                    Sala.builder().capacidad("200").horaDisponible("08:00:00")
+                    .nombre("Sala1").piso("2").build(),
+                    Sala.builder().capacidad("200").horaDisponible("10:00:00")
+                    .nombre("Sala2").piso("1").build()
+            ).flatMap(p-> repository.save(p));
 
-	            books.thenMany(repository.findAll())
-	                    .subscribe(System.out::println);
+            salas.thenMany(repository.findAll())
+                    .subscribe(System.out::println);
 
-	        };
-	    }
+        };
+    }
 }
